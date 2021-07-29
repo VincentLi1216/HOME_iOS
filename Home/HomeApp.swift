ab39c9e21b34ae40d99011cb0532084e057ea92a
Vincent <sunnus.tw@gmail.com>
2021-07-29 15:17:54 +0800

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by 李善得 on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
